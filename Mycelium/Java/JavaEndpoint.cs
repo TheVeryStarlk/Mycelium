9cd3cc77df7e7595929ff04edf70d9421c9b9232
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-18 03:42:41 +0300

    Placeholder Java map.

---
+++ Mycelium/Java/JavaEndpoint.cs
@@ -0,0 +1,17 @@
+﻿namespace Mycelium.Java;
+
+internal static class JavaEndpoint
+{
+    public static void AddJava(this IServiceCollection services)
+    {
+
+    }
+
+    public static void MapJava(this WebApplication application)
+    {
+        var group = application.MapGroup("/java");
+
+        group.MapGet("/status/{address}", (string address) => "Hello, world!");
+        group.MapGet("/ping/{address}", (string address) => "Hello, world!");
+    }
+}
--- Mycelium/Program.cs
+++ Mycelium/Program.cs
@@ -1,13 +1,19 @@
+using Mycelium.Java;
+
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(0, MyceliumJsonSerializerContext.Default));
 builder.Services.AddMemoryCache(options => options.ExpirationScanFrequency = TimeSpan.FromMinutes(1));
 builder.Services.AddProblemDetails();
 
+builder.Services.AddJava();
+
 var application = builder.Build();
 
 application.UseExceptionHandler();
 application.UseStatusCodePages();
-
 application.UseHttpsRedirection();
+
+application.MapJava();
+
 application.Run();
