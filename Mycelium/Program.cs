d66f3e4ff8bdfd8f2d0891b574ae344fde8b3546
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-18 03:38:20 +0300

    Add problem details and related middlewares.

--- Mycelium/Program.cs
+++ Mycelium/Program.cs
@@ -2,8 +2,12 @@ var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(0, MyceliumJsonSerializerContext.Default));
 builder.Services.AddMemoryCache(options => options.ExpirationScanFrequency = TimeSpan.FromMinutes(1));
+builder.Services.AddProblemDetails();
 
 var application = builder.Build();
+
+application.UseExceptionHandler();
+application.UseStatusCodePages();
 
 application.UseHttpsRedirection();
 application.Run();
