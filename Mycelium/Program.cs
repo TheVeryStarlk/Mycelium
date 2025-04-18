0a842fb756bc56cf6b7f5d9d50f51010e572eabb
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-18 03:37:17 +0300

    Add memory cache.

--- Mycelium/Program.cs
+++ Mycelium/Program.cs
@@ -1,9 +1,9 @@
 var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(0, MyceliumJsonSerializerContext.Default));
+builder.Services.AddMemoryCache(options => options.ExpirationScanFrequency = TimeSpan.FromMinutes(1));
 
 var application = builder.Build();
 
 application.UseHttpsRedirection();
-
 application.Run();
