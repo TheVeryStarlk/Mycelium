8a032214bb222782d4f99000b92a958e65d93198
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-24 05:41:17 +0300

    Fix test.

--- Mycelium.Tests/ResponseTests.cs
+++ Mycelium.Tests/ResponseTests.cs
@@ -1,4 +1,7 @@
-﻿using Mycelium.Features.Bedrock;
+﻿using System.Buffers;
+using System.Text;
+using Mycelium.Features.Bedrock;
+using Mycelium.Features.Java;
 
 namespace Mycelium.Tests;
 
@@ -29,21 +32,21 @@ internal sealed class ResponseTests
     [Test]
     public void JavaResponse_TryCreate_IsCorrect()
     {
-        // const string input = """{"version":{"name":"1.21.2","protocol":768},"players":{"max":100,"online":5},"description":{"text":"Hello, world!"}}""";
-        //
-        // var success = JavaResponse.TryCreate(input, out var response);
-        //
-        // Assert.Multiple(() =>
-        // {
-        //     Assert.That(success, Is.True);
-        //
-        //     Assert.That(response?.Description, Is.EqualTo("""{"text":"Hello, world!"}}"""));
-        //
-        //     Assert.That(response?.Name, Is.EqualTo("1.21.2"));
-        //     Assert.That(response?.Version, Is.EqualTo(768));
-        //
-        //     Assert.That(response?.Maximum, Is.EqualTo(100));
-        //     Assert.That(response?.Online, Is.EqualTo(5));
-        // });
+        const string input = """{"version":{"name":"1.21.2","protocol":768},"players":{"max":100,"online":5},"description":{"text":"Hello, world!"}}""";
+
+        var success = JavaResponse.TryCreate(new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(input)), out var response);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(success, Is.True);
+
+            Assert.That(response?.Description, Is.EqualTo("""{"text":"Hello, world!"}"""));
+
+            Assert.That(response?.Name, Is.EqualTo("1.21.2"));
+            Assert.That(response?.Version, Is.EqualTo(768));
+
+            Assert.That(response?.Maximum, Is.EqualTo(100));
+            Assert.That(response?.Online, Is.EqualTo(5));
+        });
     }
 }
