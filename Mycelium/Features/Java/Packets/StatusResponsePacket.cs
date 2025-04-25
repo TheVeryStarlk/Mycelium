ba066bc28895f46f5ec983738a9e4254577a18f7
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-04-25 20:52:44 +0300

    Simplify Java's status response packet reading.

--- Mycelium/Features/Java/Packets/StatusResponsePacket.cs
+++ Mycelium/Features/Java/Packets/StatusResponsePacket.cs
@@ -7,69 +7,59 @@ namespace Mycelium.Features.Java.Packets;
 /// <summary>
 /// Represents a Minecraft status response packet.
 /// </summary>
-/// <param name="input">The <see cref="PipeReader"/> to read from.</param>
-internal struct StatusResponsePacket(PipeReader input) : IDisposable
+internal static class StatusResponsePacket
 {
-    private SequencePosition consumed;
-    private SequencePosition examined;
-
     /// <summary>
-    /// Creates a new <see cref="StatusRequestPacket"/>, reads a packet and disposes of the <see cref="StatusRequestPacket"/>.
+    /// Reads a status response.
     /// </summary>
     /// <param name="input">The <see cref="PipeReader"/> to read from.</param>
     /// <param name="token">A <see cref="CancellationToken"/> that can be used to cancel the asynchronous operation.</param>
     /// <returns>A <see cref="ValueTask"/> representing the asynchronous read operation.</returns>
-    public static async ValueTask<Result<ReadOnlySequence<byte>>> ReadAsync(PipeReader input, CancellationToken token)
-    {
-        // Revisit this.
-        using var packet = new StatusResponsePacket(input);
-        return await packet.ReadAsync(token).ConfigureAwait(false);
-    }
-
-    /// <summary>
-    /// Disposes of the <see cref="StatusRequestPacket"/> by advanced the <see cref="PipeReader"/> to the end.
-    /// </summary>
-    public void Dispose()
-    {
-        input.AdvanceTo(consumed, examined);
-    }
-
-    /// <summary>
-    /// Reads a status response.
-    /// </summary>
-    /// <param name="token">A <see cref="CancellationToken"/> that can be used to cancel the asynchronous operation.</param>
-    /// <returns>A <see cref="ValueTask"/> representing the asynchronous read operation.</returns>
     /// <exception cref="InvalidDataException">Incomplete packet.</exception>
-    private async ValueTask<Result<ReadOnlySequence<byte>>> ReadAsync(CancellationToken token)
+    public static async ValueTask<Result<ReadOnlySequence<byte>>> ReadAsync(PipeReader input, CancellationToken token)
     {
         while (true)
         {
             var result = await input.ReadAsync(token);
-
             var buffer = result.Buffer;
 
-            consumed = buffer.Start;
-            examined = buffer.End;
+            var consumed = buffer.Start;
+            var examined = buffer.End;
 
-            if (TryRead(ref buffer, out var status))
+            // Don't advance to the end of the buffer if the packet was read successfully;
+            // The buffer is needed to construct a response. There's no intention to use the reader again,
+            // so it is "safe" to not advance - not sure if this is leaking memory or is a bad practice in general, though.
+            var success = false;
+
+            try
             {
-                consumed = buffer.Start;
-                examined = consumed;
-
-                return status;
-            }
-
-            if (result.IsCompleted)
-            {
-                if (buffer.Length > 0)
+                if (TryRead(ref buffer, out var status))
                 {
-                    return Result.Failure<ReadOnlySequence<byte>>("Incomplete packet.");
+                    consumed = buffer.Start;
+                    examined = consumed;
+
+                    success = true;
+
+                    return status;
                 }
 
-                break;
-            }
+                if (result.IsCompleted)
+                {
+                    if (buffer.Length > 0)
+                    {
+                        return Result.Failure<ReadOnlySequence<byte>>("Incomplete packet.");
+                    }
 
-            input.AdvanceTo(consumed, examined);
+                    break;
+                }
+            }
+            finally
+            {
+                if (!success)
+                {
+                    input.AdvanceTo(consumed, examined);
+                }
+            }
         }
 
         return Result.Failure<ReadOnlySequence<byte>>("Failed to read the packet.");
