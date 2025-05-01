cc0a8bc970ec4ed26400fce24162bea2735c0a4a
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-05-01 23:26:19 +0300

    Add address tests.

---
+++ Mycelium.Tests/AddressTests.cs
@@ -0,0 +1,19 @@
+﻿using Mycelium.Features;
+
+namespace Mycelium.Tests;
+
+internal sealed class AddressTests
+{
+    [Test]
+    public void Address_TryParse_IsCorrect()
+    {
+        const string input = "mc.hypixel.net:25565";
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(Address.TryParse(input, out var address), Is.True);
+            Assert.That(address.Host, Is.EqualTo("mc.hypixel.net"));
+            Assert.That(address.Port, Is.EqualTo(25565));
+        });
+    }
+}
