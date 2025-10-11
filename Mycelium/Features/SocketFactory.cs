46b6f6912e0f3f99352a452ac28cb3bcc5221c50
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-10-11 04:23:57 +0300

    Catch OCEs for socket connection.

--- Mycelium/Features/SocketFactory.cs
+++ Mycelium/Features/SocketFactory.cs
@@ -24,7 +24,7 @@ internal sealed class SocketFactory
         {
             await socket.ConnectAsync(address.Host, address.Port, token);
         }
-        catch (SocketException)
+        catch (Exception exception) when (exception is SocketException or OperationCanceledException)
         {
             socket.Dispose();
             return Result.Failure<Socket>("Could not establish a connection.");
