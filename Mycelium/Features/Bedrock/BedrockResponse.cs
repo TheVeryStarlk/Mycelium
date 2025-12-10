48e15ebe037d3ebbcbe538d191bd2428163c288f
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-10 03:06:19 +0300

    Simplify Bedrock status response reading a bit

--- Mycelium/Features/Bedrock/BedrockResponse.cs
+++ Mycelium/Features/Bedrock/BedrockResponse.cs
@@ -1,41 +1,41 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using CommunityToolkit.HighPerformance;
 
 namespace Mycelium.Features.Bedrock;
 
 /// <summary>
 /// Represents a Minecraft server status.
 /// </summary>
-/// <param name="description">Server's two lines of message of the day as known as MOTD.</param>
-/// <param name="name">Server software's name.</param>
-/// <param name="version">Supported protocol version.</param>
-/// <param name="maximum">Maximum player count.</param>
-/// <param name="online">Current online player count.</param>
-internal sealed class BedrockResponse(string[] description, string? name, int version, int maximum, int online)
+internal sealed class BedrockResponse
 {
     /// <summary>
     /// Server's two lines of message of the day as known as MOTD.
     /// </summary>
-    public string[] Description => description;
+    public string[] Description { get; set; } =
+    [
+        string.Empty,
+        string.Empty
+    ];
 
     /// <summary>
     /// Server software's name.
     /// </summary>
-    public string? Name => name;
+    public string? Name { get; set; }
 
     /// <summary>
     /// Supported protocol version.
     /// </summary>
-    public int Version => version;
+    public int Version { get; set; }
 
     /// <summary>
     /// Maximum player count.
     /// </summary>
-    public int Maximum => maximum;
+    public int Maximum { get; set; }
 
     /// <summary>
     /// Current online player count.
     /// </summary>
-    public int Online => online;
+    public int Online { get; set; }
 
     /// <summary>
     /// Tries to create a <see cref="BedrockResponse"/> from a <see cref="string"/>.
@@ -45,72 +45,66 @@ internal sealed class BedrockResponse(string[] description, string? name, int ve
     /// <returns>True if the <see cref="string"/> was converted successfully, otherwise, false.</returns>
     public static bool TryCreate(ReadOnlySpan<char> input, [NotNullWhen(true)] out BedrockResponse? response)
     {
-        response = null;
+        // Edition;MOTD line 1;Protocol Version;Version Name;Player Count;Max Player Count;Server Unique ID;MOTD line 2;Game mode;Game mode;Port (IPv4);Port (IPv6);
+        // MCPE;Dedicated Server;390;1.14.60;0;10;13253860892328930865;Bedrock level;Survival;1;19132;19133;
+
+        response = new BedrockResponse();
 
         const char separator = ';';
 
-        var count = input.Count(separator);
-
-        if (count < 8)
-        {
-            return false;
-        }
-
-        var description = new string[2];
-        var name = string.Empty;
-        var version = 0;
-        var maximum = 0;
-        var online = 0;
-
-        var parts = input.Split(separator);
         var index = 0;
 
-        foreach (var part in parts)
+        foreach (var token in input.Tokenize(separator))
         {
             switch (index)
             {
                 case 1:
-                    description[0] = input[part].ToString();
+                    response.Description[0] = token.ToString();
                     break;
 
                 case 2:
-                    if (!int.TryParse(input[part], out version))
+                    if (!int.TryParse(token, out var version))
                     {
                         return false;
                     }
+
+                    response.Version = version;
 
                     break;
 
                 case 3:
-                    name = input[part].ToString();
+                    response.Name = token.ToString();
                     break;
 
                 case 4:
-                    if (!int.TryParse(input[part], out online))
+                    if (!int.TryParse(token, out var online))
                     {
                         return false;
                     }
+
+                    response.Online = online;
 
                     break;
 
                 case 5:
-                    if (!int.TryParse(input[part], out maximum))
+                    if (!int.TryParse(token, out var maximum))
                     {
                         return false;
                     }
 
+                    response.Maximum = maximum;
+
                     break;
 
                 case 7:
-                    description[1] = input[part].ToString();
+                    response.Description[1] = token.ToString();
                     break;
             }
 
             index++;
         }
 
-        response = new BedrockResponse(description, name, version, maximum, online);
-
-        return true;
+        // Have all parts been read?
+        return index > 7;
     }
 }
