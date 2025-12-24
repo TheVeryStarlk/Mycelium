26e9b6d11fd86063cbe1e207858d0fedb63222da
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-24 23:34:37 +0300

    Create convenience overload

--- Mycelium/Editions/Bedrock/BedrockClient.cs
+++ Mycelium/Editions/Bedrock/BedrockClient.cs
@@ -10,7 +10,21 @@ public sealed class BedrockClient(ISocketFactory factory)
         // What about logging?
     }
 
-    public async Task<string?> RequestStatusAsync(Address address, CancellationToken token)
+    public async Task<BedrockResponse?> RequestStatusAsync(string address, CancellationToken token = default)
+    {
+        if (!Address.TryParse(address, out var result))
+        {
+            throw new MyceliumException("Invalid address.");
+        }
+
+        var status = await RequestStatusAsync(result, token);
+
+        return !BedrockResponse.TryCreate(status, out var response) 
+            ? throw new MyceliumException("Received invalid status.") 
+            : response;
+    }
+    
+    public async Task<string?> RequestStatusAsync(Address address, CancellationToken token = default)
     {
         using var socket = await factory.ConnectAsync(address, SocketType.Dgram, ProtocolType.Udp, token);
 
