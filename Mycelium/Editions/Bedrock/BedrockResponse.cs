6ee811652af120870652ae2a257389a284b0a8d3
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-24 23:29:47 +0300

    Implement Bedrock response parsing

--- Mycelium/Editions/Bedrock/BedrockResponse.cs
+++ Mycelium/Editions/Bedrock/BedrockResponse.cs
@@ -20,7 +20,62 @@ public sealed class BedrockResponse
 
     public static bool TryCreate(ReadOnlySpan<char> input, [NotNullWhen(true)] out BedrockResponse? response)
     {
-        response = null;
-        return false;
+        response = new BedrockResponse();
+        
+        var parts = input.Split(':');
+        var index = 0;
+
+        foreach (var part in parts)
+        {
+            switch (index)
+            {
+                case 1:
+                    response.Description[0] = input[part].ToString();
+                    break;
+
+                case 2:
+                    if (!int.TryParse(input[part], out var version))
+                    {
+                        return false;
+                    }
+
+                    response.Version = version;
+                    
+                    break;
+
+                case 3:
+                    response.Name = input[part].ToString();
+                    break;
+
+                case 4:
+                    if (!int.TryParse(input[part], out var online))
+                    {
+                        return false;
+                    }
+
+                    response.Online = online;
+
+                    break;
+
+                case 5:
+                    if (!int.TryParse(input[part], out var maximum))
+                    {
+                        return false;
+                    }
+
+                    response.Maximum = maximum;
+                    
+                    break;
+
+                case 7:
+                    response.Description[1] = input[part].ToString();
+                    break;
+            }
+
+            index++;
+        }
+
+        // Have all parts been read?
+        return index > 7;
     }
 }
