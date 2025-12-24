ea65a0979aec059c0c289f22a38ff0c928fe6573
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-24 23:42:56 +0300

    Simplify socket factory

--- Mycelium/ISocketFactory.cs
+++ Mycelium/ISocketFactory.cs
@@ -6,3 +6,15 @@ public interface ISocketFactory
 {
     public Task<Socket> ConnectAsync(Address address, SocketType type, ProtocolType protocol, CancellationToken token);
 }
+
+internal sealed class SocketFactory : ISocketFactory
+{
+    public async Task<Socket> ConnectAsync(Address address, SocketType type, ProtocolType protocol, CancellationToken token)
+    {
+        var socket = new Socket(type, protocol);
+
+        await socket.ConnectAsync(address.Host, address.Port, token);
+
+        return socket;
+    }
+}
