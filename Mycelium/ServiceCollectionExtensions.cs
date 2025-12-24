d9b19f3675f00b9923373cd6d57c0a95de586514
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-24 23:35:46 +0300

    Add service collection extensions

---
+++ Mycelium/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Mycelium.Editions;
+using Mycelium.Editions.Bedrock;
+
+namespace Mycelium;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddMycelium(this IServiceCollection services)
+    {
+        services.AddSingleton<ISocketFactory, SocketFactory>();
+        services.AddTransient<BedrockClient>();
+
+        return services;
+    }
+}
