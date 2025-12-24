b626304ef3eda645038b15858913ea8d476d217c
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-24 23:39:37 +0300

    Start working on Java client

---
+++ Mycelium/Java/JavaClient.cs
@@ -0,0 +1,9 @@
+﻿namespace Mycelium.Java;
+
+public sealed class JavaClient(ISocketFactory factory)
+{
+    public JavaClient() : this(new SocketFactory())
+    {
+        // What about logging?
+    }
+}
