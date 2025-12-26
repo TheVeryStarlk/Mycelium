8f8b7c0f65a3d88456d40e5675847f6b74c60942
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-26 23:47:04 +0300

    Add some benchmarks

---
+++ Mycelium.Benchmarks/VariableLengthBenchmarks.cs
@@ -0,0 +1,48 @@
+﻿using BenchmarkDotNet.Attributes;
+using Mycelium.Java.Packets;
+
+namespace Mycelium.Benchmarks;
+
+public sealed class VariableLengthBenchmarks
+{
+    [Benchmark]
+    [Arguments(256)]
+    [Arguments(65536)]
+    [Arguments(16777216)]
+    [Arguments(2147483647)]
+    public int GetIntegerByteCount(int value)
+    {
+        return VariableLength.GetByteCount(value);
+    }
+    
+    [Benchmark]
+    [Arguments("Foo")]
+    [Arguments("Foo Bar")]
+    [Arguments("Foo Bar Baz")]
+    [Arguments("Foo Bar Baz Qux")]
+    public int GetIntegerByteCount(string value)
+    {
+        return VariableLength.GetByteCount(value);
+    }
+
+    [Benchmark]
+    [Arguments(256)]
+    [Arguments(65536)]
+    [Arguments(16777216)]
+    [Arguments(2147483647)]
+    public int WriteInteger(int value)
+    {
+        Span<byte> span = stackalloc byte[sizeof(int) + sizeof(byte)];
+        return VariableLength.Write(span, value);
+    }
+    
+    [Benchmark]
+    [Arguments("Foo")]
+    [Arguments("Foo Bar")]
+    [Arguments("Foo Bar Baz")]
+    [Arguments("Foo Bar Baz Qux")]
+    public int WriteString(string value)
+    {
+        return VariableLength.GetByteCount(value);
+    }
+}
---
+++ Mycelium/InternalsVisibleTo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Mycelium.Benchmarks")]
+[assembly: InternalsVisibleTo("Mycelium.Tests")]
