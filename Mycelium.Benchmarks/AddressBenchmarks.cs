bc47b0f5d73a3e6c73dabe343676e4ea327d3818
TheVeryStarlk <starlkytminecraft@gmail.com>
2025-12-26 23:37:47 +0300

    Add a benchmarks library

---
+++ Mycelium.Benchmarks/AddressBenchmarks.cs
@@ -0,0 +1,20 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Mycelium.Benchmarks;
+
+public sealed class AddressBenchmarks
+{
+    [Benchmark]
+    public Address Parsing()
+    {
+        _ = Address.TryParse("foo.bar.baz.qux.:25565", out var address);
+        return address;
+    }
+    
+    [Benchmark]
+    public Address ParsingShort()
+    {
+        _ = Address.TryParse("hello.world:25565", out var address);
+        return address;
+    }
+}
